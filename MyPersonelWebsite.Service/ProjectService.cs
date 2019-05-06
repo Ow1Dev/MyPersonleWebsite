2ab25305f9dd8530796c4dc4aed80e9e3b1a812a
Mads Bruun Jensen <madsj812@gmail.com>
2019-05-06 10:52:34 +0200

    adding service

---
+++ MyPersonelWebsite.Service/ProjectService.cs
@@ -0,0 +1,39 @@
+﻿using MyPersonelWebsite.Data;
+using MyPersonelWebsite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyPersonelWebsite.Service
+{
+    public class ProjectService : IProject
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            return _context.Projects
+                .Include(project => project.Tag);
+        }
+
+        public Project GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyPersonelWebsite.data/IProject.cs
@@ -0,0 +1,15 @@
+﻿using MyPersonelWebsite.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyPersonelWebsite.Data
+{
+    public interface IProject
+    {
+        Project GetById(int id);
+        IEnumerable<Project> GetAll();
+
+        Task Create(Project project);
+        Task Delete(Project project);
+    }
+}
